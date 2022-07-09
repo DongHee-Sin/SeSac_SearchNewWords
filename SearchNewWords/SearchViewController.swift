fb668b2a09c23c67cbc907b5e3440768aa3c6d6b
DongHee-Sin <voslr0601@naver.com>
2022-07-09 16:29:17 +0900

    신조어 예시 버튼들 UI 설정 (코드)

--- SearchNewWords/SearchViewController.swift
+++ SearchNewWords/SearchViewController.swift
@@ -9,10 +9,39 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    // MARK: - Propertys
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    @IBOutlet var newWordsExampleList: [UIButton]!
+    
+    
+    
+    // MARK: - View Did Load
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        newWordsExampleList.forEach({
+            settingExampleButtonUI($0)
+        })
     }
 
 
+    
+    // MARK: - Methods
+    func settingExampleButtonUI(_ button: UIButton) {
+        addRadius(button)
+        addBorder(button)
+        button.contentEdgeInsets = UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
+    }
+    
+    func addRadius(_ view: UIView) {
+        view.clipsToBounds = true
+        view.layer.cornerRadius = view.frame.height / 5
+    }
+    
+    func addBorder(_ view: UIView) {
+        view.layer.borderWidth = 1
+        view.layer.borderColor = UIColor.black.cgColor
+    }
 }
 
